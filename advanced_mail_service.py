5a889962abc7990d74e5909e95dfa371ace458d6
EcranBleu <nclochez@gmail.com>
2020-08-11 13:57:41 +0200

    new files

--- advanced_mail_service.py
+++ advanced_mail_service.py
@@ -5,15 +5,15 @@ from pathlib import Path
 
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
-email['from'] = 'Bidontavu'
-email['to'] = 'bidontavu@gmail.com'
-email['subject'] = 'Dummy subject'
+email['from'] = #Sender name
+email['to'] = #Destination email
+email['subject'] = #Mail subject
 
 email.set_content(html.substitute(name='TestUser'), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp_host:
     smtp_host.ehlo()
     smtp_host.starttls()
-    smtp_host.login('bidontavu@gmail.com', 'Champignon:visceral!Wahl')
+    smtp_host.login(#youremailaccount, #yourpassword)
     smtp_host.send_message(email)
     print(f'Mail sent to {email["to"]}')
